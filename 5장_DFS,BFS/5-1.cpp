4a957ade1541f84c8400ff76140961bbdb2542b8
JIYEON-LAPTOP <kjy6339@naver.com>
2021-07-18 21:13:55 +0900

    DFS 알고리즘의 기본

---
+++ 5장_DFS,BFS/5-1.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool visited[9];
+vector<int> graph[9];
+//int 형이 들어가는 graph라는 vector를 여러개 만들었음(각각이 하나의 노드에서 시작하는 노선의 출발점이 됨) 
+
+// DFS 함수 정의
+void dfs(int x) {
+    // 현재 노드를 방문 처리
+    visited[x] = true;
+    cout << x << ' ';
+    // 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for (int i = 0; i < graph[x].size(); i++) {
+        int y = graph[x][i]; //y는 다음에 방문할 노드의 번호 
+        if (!visited[y]) dfs(y); //y 노드가 방문하지 않았다면 함수를 호출 
+    }
+}
+
+int main(void) {
+    // 노드 1에 연결된 노드 정보 저장 
+    graph[1].push_back(2);
+    graph[1].push_back(3);
+    graph[1].push_back(8);
+    
+    // 노드 2에 연결된 노드 정보 저장 
+    graph[2].push_back(1);
+    graph[2].push_back(7);
+    
+    // 노드 3에 연결된 노드 정보 저장 
+    graph[3].push_back(1);
+    graph[3].push_back(4);
+    graph[3].push_back(5);
+    
+    // 노드 4에 연결된 노드 정보 저장 
+    graph[4].push_back(3);
+    graph[4].push_back(5);
+    
+    // 노드 5에 연결된 노드 정보 저장 
+    graph[5].push_back(3);
+    graph[5].push_back(4);
+    
+    // 노드 6에 연결된 노드 정보 저장 
+    graph[6].push_back(7);
+    
+    // 노드 7에 연결된 노드 정보 저장 
+    graph[7].push_back(2);
+    graph[7].push_back(6);
+    graph[7].push_back(8);
+    
+    // 노드 8에 연결된 노드 정보 저장 
+    graph[8].push_back(1);
+    graph[8].push_back(7);
+    
+    dfs(1);
+}
